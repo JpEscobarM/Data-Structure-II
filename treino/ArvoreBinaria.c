c01c0eaa9996f99aefee3a43689ea638284fb177
joao paulo <jp.escobarmartins@gmail.com>
2025-08-09 16:46:29 -0300

    Update ArvoreBinaria.c

--- treino/ArvoreBinaria.c
+++ treino/ArvoreBinaria.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>>
+
 
 typedef struct no
 {
@@ -46,7 +48,81 @@ No* inserirV1(No **raiz, int num){
 
 }
 
-void imprimirV1(No **raiz)
+void inserirV2(No **raiz,int num){
+
+    if(*raiz == NULL)
+    {
+
+     *raiz = malloc(sizeof(No));
+     (*raiz)->num = num;
+     (*raiz)->dir = NULL;
+     (*raiz)->esq = NULL;
+    }
+    else
+    {
+
+       if(num < (*raiz)->num)
+        {
+
+         inserirV2(&(*raiz)->esq,num);
+
+        }
+        else{
+            inserirV2(&(*raiz)->dir,num);
+        }
+    }
+
+}
+
+void inserirV3(No **raiz, int num)
+{
+    No *aux = *raiz;
+
+
+    while(aux)
+    {
+        if(num < aux->num) raiz = &(aux->esq);
+        else raiz = &(aux->dir);
+
+        aux = *raiz;
+    }
+    aux = malloc(sizeof(No));
+    aux->num = num;
+    aux->dir = NULL;
+    aux->esq = NULL;
+
+    *raiz = aux;
+
+}
+
+
+//BUSCAS
+No* buscarV1(No **raiz,int num)
+{
+    if(*raiz)
+    {
+        if(num == (*raiz)->num)
+        {
+            return *raiz;
+        }
+        else if (num < (*raiz)->num)
+        {
+            return buscarV1(&(*raiz)->esq, num);
+        }
+        else
+        {
+            return buscarV1(&(*raiz)->dir,num);
+        }
+    }
+    else
+    {
+
+        return NULL;
+    }
+}
+
+
+void imprimirV1(No **raiz) //IMPRESSAO PRÉ ORDEM
 {
     if(*raiz)
     {
@@ -58,7 +134,7 @@ void imprimirV1(No **raiz)
 
 }
 
-void imprimirV2(No **raiz)
+void imprimirV2(No **raiz) //ORDENADA
 {
     if(*raiz)
     {
@@ -71,24 +147,83 @@ void imprimirV2(No **raiz)
 
 }
 
+int altura (No **raiz)
+{
 
-int
-main
-(
- )
- {
+    if(*raiz == NULL)
+    {
+        return -1;
+    }
+    else{
+        int esq = altura(&(*raiz)->esq);
+        int dir = altura(&(*raiz)->dir);
+     return (esq > dir ? esq : dir) + 1 ;
+    }
+}
+
+
+int main(){
 
  No *raiz = NULL;
+ No *busca = NULL;
+     int op,valor;
+
+   srand(time(NULL));
+
+    for(int i = 0 ; i < 30; i ++)
+    {
+
+        inserirV2(&raiz,rand()/100);
+    }
+
+ op = 1;
 
 
 
-    inserirV1(&raiz,2);
-    inserirV1(&raiz,3);
-    inserirV1(&raiz,1);
+    while(op != 0 )
+ {
+     printf("\nEscolha uma opcao: ");
+     printf("\n 0 - SAIR | 1 - INSERIR | 2 - IMPRIMIR | 3 - BUSCAR | 4 - ALTURA\n");
+    scanf("%d", &op);
+     switch (op){
+    case 0:
+     printf("\nSaindo...");
+    break;
+     case 1:
+        printf("\nDIGITE O VALOR:_");
+        scanf("%d", &valor);
+        inserirV3(&raiz,valor);
+
+    break;
+     case 2:
+        imprimirV2(&raiz);
+    break;
+     case 3:
+    printf("\nDIGITE O VALOR A SER BUSCADO:_");
+        scanf("%d", &valor);
+        busca = buscarV1(&raiz,valor);
+
+        if(busca)
+        {
+            printf("\nValor encontrado: %d", busca->num);
+        }
+        else
+        {
+
+            printf("\nValor nao encontrado na arvore");
+        }
+
+    break;
+     case 4:
+        printf("\n ALTURA: %d", altura(&raiz));
+    break;
 
 
+     }
 
-    imprimirV1(&raiz);
+ }
+
+
 
 return 0;
 }
